03de0ffed436bc1afeafb7b204edb5956a4d4d6d
internethealthtest <noreply@internethealthtest.org>
2015-07-14 23:06:46 -0400

    ndtfix

--- resources/scripts/ndt.js
+++ resources/scripts/ndt.js
@@ -681,27 +681,28 @@ NDTjs.prototype.startTest = function () {
 		module.exports = NDTjs;
 		WebSocket = require('ws');
     }
-	if ('ArrayBuffer' in window) {
-	  if (!ArrayBuffer.prototype.slice) {
-		ArrayBuffer.prototype.slice = function (begin, end) {
-		  var len = this.byteLength;
-		  begin = (begin|0) || 0;
-		  end = end === (void 0) ? len : (end|0);
-
-		  // Handle negative values.
-		  if (begin < 0) begin = Math.max(begin + len, 0);
-		  if (end < 0) end = Math.max(end + len, 0);
-
-		  if (len === 0 || begin >= len || begin >= end) {
-			return new ArrayBuffer(0);
-		  }
-
-		  var length = Math.min(len - begin, end - begin);
-		  var target = new ArrayBuffer(length);
-		  var targetArray = new Uint8Array(target);
-		  targetArray.set(new Uint8Array(this, begin, length));
-		  return target;
-		};
-	  }
-	}
 })();
+
+if ('ArrayBuffer' in window) {
+  if (!ArrayBuffer.prototype.slice) {
+	ArrayBuffer.prototype.slice = function (begin, end) {
+	  var len = this.byteLength;
+	  begin = (begin|0) || 0;
+	  end = end === (void 0) ? len : (end|0);
+
+	  // Handle negative values.
+	  if (begin < 0) begin = Math.max(begin + len, 0);
+	  if (end < 0) end = Math.max(end + len, 0);
+
+	  if (len === 0 || begin >= len || begin >= end) {
+		return new ArrayBuffer(0);
+	  }
+
+	  var length = Math.min(len - begin, end - begin);
+	  var target = new ArrayBuffer(length);
+	  var targetArray = new Uint8Array(target);
+	  targetArray.set(new Uint8Array(this, begin, length));
+	  return target;
+	};
+  }
+}
